30fdc06ab48ee5bacf4e769a8a214554f8a8e649
AhmedSalahAamer <104269198+AhmedSalahAamer@users.noreply.github.com>
2023-04-25 15:27:25 +0200

    Step two

--- app/src/main/java/com/example/quizapp/Constants.kt
+++ app/src/main/java/com/example/quizapp/Constants.kt
@@ -11,6 +11,33 @@ object Constants {
             1
         )
         questionList.add(Q1)
+
+        val Q2 = Questions(
+            1,
+            "What country does this flag belong to?",
+            R.drawable.ic_bg,
+            "Egypt","Marcoo","Tonis","Saudi Arabia",
+            3
+        )
+        questionList.add(Q2)
+
+        val Q3 = Questions(
+            1,
+            "What country does this flag belong to?",
+            R.drawable.ic_bg,
+            "Egypt","Marcoo","Tonis","Saudi Arabia",
+            2
+        )
+        questionList.add(Q3)
+
+        val Q4 = Questions(
+            1,
+            "What country does this flag belong to?",
+            R.drawable.ic_bg,
+            "Egypt","Marcoo","Tonis","Saudi Arabia",
+            4
+        )
+        questionList.add(Q4)
         return questionList
     }
 }
--- app/src/main/java/com/example/quizapp/QuestionsActivity.kt
+++ app/src/main/java/com/example/quizapp/QuestionsActivity.kt
@@ -2,10 +2,24 @@ package com.example.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.quizapp.databinding.ActivityQuastionsBinding
 
 class QuestionsActivity : AppCompatActivity() {
+    lateinit var binding:ActivityQuastionsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_quastions)
+        binding = ActivityQuastionsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val questionList = Constants.getQuestions()
+        Log.i("QuestionList size is","${questionList.size}")
+
+        var currentPosition = 0
+        val question : Questions = questionList[currentPosition]
+        binding.progressBar.progress = currentPosition
+        binding.tvProgress.text = "$currentPosition/${binding.progressBar.max}"
+
+
     }
 }
